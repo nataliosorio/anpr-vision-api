ce39db814c53cd0ea551010ee261c48dc8aad5f1
nataliosorio <nataliaosorio973@gmail.com>
2025-10-26 18:20:47 -0500

    initial

--- Api/Business/Interfaces/Detection/IVehicleDetectionManagerBusiness.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Entity.Records;
-
-namespace Business.Interfaces.Detection;
-
-public interface IVehicleDetectionManagerBusiness
-{
-    Task ProcessDetectionAsync(PlateDetectedEventRecord evt,CancellationToken cancellationToken = default);
-}
--- Api/Business/Interfaces/Operational/INotificationDispatcher.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Entity.Dtos.Operational;
-
-namespace Business.Interfaces.Operational;
-
-public interface INotificationDispatcher
-{
-    Task SendAsync(int parkingId, NotificationDto notification);
-}
