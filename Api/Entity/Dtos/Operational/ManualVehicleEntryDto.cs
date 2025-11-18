7ce3e34efa1b1bc1002897f0b33b0daf130f6737
nataliosorio <nataliaosorio973@gmail.com>
2025-11-18 10:10:51 -0500

    registro de entrada manual

---
+++ Api/Entity/Dtos/Operational/ManualVehicleEntryDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Dtos.Operational
+{
+    public class ManualVehicleEntryDto
+    {
+        [Required(ErrorMessage = "La Placa es requerida.")]
+        public string Plate { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El ID del parqueadero es requerido.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El ParkingId debe ser un valor válido.")]
+        public int ParkingId { get; set; }
+
+        [Required(ErrorMessage = "El ID del tipo de vehículo es requerido.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El TypeVehicleId debe ser mayor a 0.")]
+        public int TypeVehicleId { get; set; }
+    }
+}
