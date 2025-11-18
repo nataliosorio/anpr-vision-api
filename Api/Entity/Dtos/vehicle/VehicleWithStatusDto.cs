031c5630a1acd6e535273292c74f09ec26b5a490
nataliosorio <nataliaosorio973@gmail.com>
2025-11-18 02:19:33 -0500

    metodos-app

---
+++ Api/Entity/Dtos/vehicle/VehicleWithStatusDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity.Dtos.Operational;
+
+namespace Entity.Dtos.vehicle
+{
+    public class VehicleWithStatusDto : VehicleDto
+    {
+
+        public bool IsInside { get; set; }
+
+        public DateTime? EntryDate { get; set; }
+        public string? SlotName { get; set; }
+        public int? SlotId { get; set; }
+
+        public string? TimeInside { get; set; } 
+    }
+}
