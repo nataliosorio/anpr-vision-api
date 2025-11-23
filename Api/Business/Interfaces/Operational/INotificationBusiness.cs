454246e4d8de54ef08d62fc5daa818e4f73cf6e0
nataliosorio <nataliaosorio973@gmail.com>
2025-11-22 23:20:54 -0500

    feat(user): agregar la funcionalidad de marcar todas las notificaciones como leidas

--- Api/Business/Interfaces/Operational/INotificationBusiness.cs
+++ Api/Business/Interfaces/Operational/INotificationBusiness.cs
@@ -8,6 +8,7 @@ public interface INotificationBusiness : IRepositoryBusiness<Notification, Notif
 {
     Task<List<NotificationDto>> GetByParkingAsync(int parkingId, bool onlyUnread = false);
     Task MarkAsReadAsync(int id);
+    Task MarkAllAsReadByParkingAsync(int parkingId);
     Task<NotificationDto> CreateAndNotifyAsync(NotificationDto dto);
     Task<NotificationDto> EnqueueAndNotifyAsync(NotificationDto dto);
 }
